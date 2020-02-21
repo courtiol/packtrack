160922877f0570c08ed806e1af148e3a4f408182
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2020-02-21 19:20:14 +0100

    New arguments in pktk_view() to list previously loaded pkgs

--- R/view_data.R
+++ R/view_data.R
@@ -1,17 +1,24 @@
 #' View the data
 #'
-#' This function display information about the usage of the packages.
+#' This function displays information about the usage of the packages.
 #'
-#' @param include_non_used a boolean indicating whether to add (TRUE) or not
-#' (FALSE, default) information about user packages not recorded during
-#' tracking.
+#' @param previously_loaded boolean indicating whether to add (TRUE) or not
+#' (FALSE) information about packages that were already loaded before
+#' that the tracking was activated. This includes for instance the packtrack package
+#' itself. Default is to do the same as the argument selected for non_used.
+#'
+#' @param non_used a boolean indicating whether to add (TRUE) or not
+#' (FALSE, default) information about packages not recorded during tracking.
+#'
+#'
 #'
 #' @export
 #'
 #' @examples
 #'  pktk_view()
 #'
-pktk_view <- function(include_non_used = FALSE) {
+pktk_view <- function(previously_loaded = non_used, non_used = FALSE) {
+
   ## extract last time a package has been used and set time zone:
   dat_time <- .pktk_data$time$data.frame()
   dat_time$Values <- format(dat_time$Values, tz =  Sys.timezone())
@@ -23,16 +30,23 @@ pktk_view <- function(include_non_used = FALSE) {
   dat <- merge(dat_time, dat_n, by = "Keys")
 
   ## prettify the output:
-  colnames(dat) <- c("package", "last_loaded", "times_used")
+  colnames(dat) <- c("package", "last_loaded", "times_loaded")
   dat <- dat[order(dat$package), ]
   rownames(dat) <- NULL
 
-  ## include non used packages
-  if (include_non_used) {
-    all_pkg <- data.frame(package = dir(.libPaths()))
-    dat <- merge(all_pkg[!all_pkg$package %in% dat$package, , drop = FALSE],
-                 dat, by = "package", all = TRUE)
-    dat$times_used[is.na(dat$times_used)] <- 0
+  ## include previously loaded packages:
+  if (previously_loaded) {
+    all_loaded_pkg <- data.frame(package = loadedNamespaces(), last_loaded = NA,  times_loaded = 1)
+    dat <- merge(all_loaded_pkg, dat, all.x = TRUE, all.y = TRUE)
+  }
+
+  ## include non-used packages:
+  if (non_used) {
+    if (!previously_loaded) {
+      stop("Using non_used = TRUE together with previously_loaded = FALSE would not make sense because packages already loaded when the tracking has started would be considered as never used. Consider changing the input of the function!")
+    }
+    all_pkg <- data.frame(package = dir(.libPaths()), last_loaded = NA,  times_loaded = 0)
+    dat <- merge(all_pkg, dat, all.x = TRUE, all.y = TRUE)
   }
 
   ## return the output:
